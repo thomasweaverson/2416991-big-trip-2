ecc3d45e284da3da09638688a743e979abea26e1
Thomas <thomasweaverson@gmail.com>
2026-05-26 09:34:51 +0300

    Раздел 8. Задание 2. Добавит поддержку обратной связи в презентере доски

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -85,12 +85,15 @@ export default class TripPresenter {
   #handleViewAction = (actionType, updateType, update) => {
     switch (actionType) {
       case UserAction.UPDATE_POINT:
+        this.#pointPresenters.get(update.id).setSaving();
         this.#pointsModel.updatePoint(updateType, update);
         break;
       case UserAction.ADD_POINT:
+        this.#pointPresenters.get(update.id).setSaving();
         this.#pointsModel.addPoint(updateType, update);
         break;
       case UserAction.DELETE_POINT:
+        this.#pointPresenters.get(update.id).setDeleting();
         this.#pointsModel.deletePoint(updateType, update);
         break;
     }

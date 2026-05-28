7d6efed05318797d408ef898c171cc57c9c006fd
Thomas <thomasweaverson@gmail.com>
2026-05-28 09:05:34 +0300

    Раздел 8. Задание 3. Исправит обработку ошибок у формы новой точки

--- src/presenter/new-point-presenter.js
+++ src/presenter/new-point-presenter.js
@@ -90,7 +90,6 @@ export default class NewPointPresenter {
       UpdateType.MINOR,
       pointToAdd
     );
-    this.destroy();
   };
 
   #handleFormReset = () => {
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -13,13 +13,13 @@ const isPointsEqual = (pointA, pointB) => {
   return isBasePriceEqual && isDatesSame && isDestinationEqual && isFavoriteEqual && isOffersEqual && isTypeEqual;
 };
 
-const isNumber = (value) =>
+const isCorrectNumber = (value) =>
   !isNaN(+value) && value !== '' &&
   String(+value) === String(value) &&
   Number.isInteger(+value);
 
 const isPointDataValid = (point, destination, destinations) => {
-  const isBasePriceValid = isNumber(point.basePrice) && point.basePrice > 0;
+  const isBasePriceValid = isCorrectNumber(point.basePrice) && point.basePrice > 0 && point.basePrice <= 100000;
   const isDestinationValid =
     destinations.some((destinationItem) => destinationItem.id === destination.id) &&
     destinations.some((destinationItem) => destinationItem.id === point.destination);
